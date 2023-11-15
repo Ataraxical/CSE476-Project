a538d95ce7ff33155ef76ed14e24536c4bc6571c
Daniel Taeyoung Sohn <sohn.d.50@gmail.com>
2023-11-14 23:28:20 -0500

    Removed recommended, made home default fragment

--- app/src/main/java/com/gamealike/CSE476_Project/ui/recommended/RecommendedViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.gamealike.CSE476_Project.ui.recommended;
-
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class RecommendedViewModel extends ViewModel {
-
-}
